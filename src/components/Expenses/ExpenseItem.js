21211db95cd46b28d326f2010e43d5eea45b8d24
shindetejas08 <61664348+shindetejas08@users.noreply.github.com>
2023-07-20 22:16:34 +0530

    expense-tracker-v3-sec6

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,21 @@
-import React,{useState} from "react"
+import React from "react"
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card"
 
 const ExpenseItem = (props)=>{
-    const {date, amount} = props.item
-    const [title,setTitle] = useState(props.item.title)
-
-    const handleClick = ()=>{
-        setTitle("Updated!!!")
-    }
+    const {date,title, amount} = props.item
+    
     return(
-
-        <Card className="expense-item">
-            <ExpenseDate date={date}/> 
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${amount}</div>
-                <button onClick={handleClick}>ClickMe</button>
-            </div>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={date}/> 
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                    <div className="expense-item__price">${amount}</div>
+                </div>
+            </Card>
+        </li>
     )
 }
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -43,10 +43,9 @@ const ExpenseForm = (props)=>{
         e.preventDefault()
         const expenseData = {
             title: enteredTitle,
-            amount:enteredAmount,
-            date: new Date(enteredDate)
-        }
-
+            amount: +enteredAmount,
+            date: new Date(enteredDate),
+          };
         props.onSaveExpenseData(expenseData)
         setEnteredTitle("")
         setEnteredAmount("")
@@ -70,6 +69,7 @@ const ExpenseForm = (props)=>{
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
